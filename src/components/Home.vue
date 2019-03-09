37c6ec8048ae7c10445f4760f3968c0649b3c922
Kusal <kusalekanayake@yahoo.co.nz>
2019-03-10 11:49:21 +1300

    Init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <p>
+            The start of Kumin
+        </p>
+        <h3>Installed CLI Plugins</h3>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
---
+++ src/components/Ku.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ku"
+    }
+</script>
+
+<style scoped>
+
+</style>
